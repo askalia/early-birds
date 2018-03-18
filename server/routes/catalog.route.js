1243d862435e8b58d820cc4006566ba89e1e934a
Joris Grouillet <joris@askalia.net>
2018-03-18 11:35:16 +0100

    express-validation and validators  not currently required. but will be.

--- server/routes/catalog.route.js
+++ server/routes/catalog.route.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import paramValidation from '../../config/param-validation';
 import catalogCtrl from '../controllers/catalog.ctrl';
 
 const router = express.Router();
